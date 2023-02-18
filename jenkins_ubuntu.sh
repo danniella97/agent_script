55ac9615817c16520bcff286335a815e7de848aa
daniella <daniellakitio97@gmail.com>
2023-02-18 13:28:32 -0500

    agent script

--- jenkins_ubuntu.sh
+++ jenkins_ubuntu.sh
@@ -3,12 +3,12 @@
 # this script is only tested on ubuntu xenial
 
 # install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+curl -fsSL httppository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
+apt-get updates://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-re
 apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
